a1b579d3eac03eeee9db74c8c427491652e3d389
Victor Irekponor <marquisvictor93@gmail.com>
2020-09-30 23:40:54 +0100

    fix act error by waiting for element to be removed in the app.test.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { TodoList } from "./TodoList";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Getting started with React testing library
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+				Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+				className="App-link"
+				href="https://reactjs.org"
+				target="_blank"
+				rel="noopener noreferrer"
+				>
+				Getting started with React testing library
+				</a>
+			</header>
+			<div className="App-body">
+				<BrowserRouter>
+					<Route exact path="/" component={TodoList} />
+				</BrowserRouter>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { render, screen } from './custom-render';
+import { render, screen, waitForElementToBeRemoved } from './custom-render';
+import "@testing-library/jest-dom"
+import "@testing-library/dom"
+import "@testing-library/react"
 import App from './App';
 
 
-// test('renders learn react link', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
 describe("the <App /> component", () => {
-	it ("renders <App /> component correctly", () => {
+	it ("renders <App /> component correctly", async () => {
 		render(<App />);
+
+		await waitForElementToBeRemoved(()=> screen.getByText(/fetching todos/i))
 		expect(screen.getByText(/Getting started with React testing library/i)).toBeInTheDocument();
 		});
 	}
