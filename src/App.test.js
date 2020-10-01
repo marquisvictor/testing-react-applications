851d0f05b66a369a695ec325c26825a649fc5f49
Victor Irekponor <marquisvictor93@gmail.com>
2020-10-01 10:56:17 +0100

    finished :rocket:

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,14 @@ import { render, screen, waitForElementToBeRemoved } from './custom-render';
 import "@testing-library/jest-dom"
 import "@testing-library/dom"
 import "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import App from './App';
+import { todos } from './makeTodos';
+import Axios from 'axios';
+
+jest.mock("react-router-dom", () => ({
+	...jest.requireActual("react-router-dom"),
+}));
 
 
 describe("the <App /> component", () => {
@@ -13,5 +20,46 @@ describe("the <App /> component", () => {
 		await waitForElementToBeRemoved(()=> screen.getByText(/fetching todos/i))
 		expect(screen.getByText(/Getting started with React testing library/i)).toBeInTheDocument();
 		});
+
+	it("renders todo, and one can click to view a todo item", async () => {
+		render(<App/ >);
+		
+		await waitForElementToBeRemoved(() => screen.getByText(/fetching todos/i));
+
+		todos.slice(0, 15).forEach((td) => {
+			expect(screen.getByText(td.title)).toBeInTheDocument();
+		});
+		// click on a todo item and test the result. 
+
+		const {id, title, userId, completed } = todos[0]
+		Axios.get.mockImplementationOnce(() => Promise.resolve({
+			data: {id, title, userId, completed},
+		}))
+		userEvent.click(screen.getByTestId(String(id)));
+		await waitForElementToBeRemoved(() =>
+		  screen.getByText(`Fetching todo item ${String(id)}`)
+		);
+
+		expect(screen.getByText(title)).toBeInTheDocument();
+		expect(screen.getByText(`Added by: ${userId}`)).toBeInTheDocument();
+		switch (completed) {
+		case true:
+			expect(
+			screen.getByText(/This item has been completed/)
+			).toBeInTheDocument();
+			break;
+		case false:
+			expect(
+			screen.getByText(/This item is yet to be completed/)
+			).toBeInTheDocument();
+			break;
+		default:
+			throw new Error("No match");
+			}
+  
+	
+	})
+
+
 	}
 );
